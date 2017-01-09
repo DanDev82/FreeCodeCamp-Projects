7bdf4ba24455da689be61deddc037ff54e9c8014
Daniel Coria <ifdanthencool@gmail.com>
2017-01-09 00:27:26 -0600

    Sl1pg8r Trubute Page
    Trying out some PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.html';?>
+</head>
+<body>
+	<!-- header -->
+	<?php include 'index-header.html';?>
+
+	<!-- body content -->
+	<?php include 'section1.html';?>
+
+	<!-- page footer -->
+	<?php include 'footer.html';?>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
+
+
---
+++ movies.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.html';?>
+</head>
+<body>
+	<?php include 'movie-header.html';?>
+	<?php include 'section2.html';?>
+	<?php include 'footer.html';?>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
+
+
