9332f04c1e19c257d0f7c36abb4d305f950f9c9d
Dan Dev <ifdanthencool@gmail.com>
2017-01-09 13:07:07 -0600

    Change layout of page

---
+++ Sl1pg8r/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.html';?>
+</head>
+<body>
+	<!-- 10px top/bottom spacing -->
+	<?php include '/space-between.html';?>
+
+	<!-- header -->
+	<?php include 'index-header.html';?>
+
+	<!-- body content -->
+	<?php include 'section1.html';?>
+
+	<!-- page footer -->
+	<?php include 'footer.html';?>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
+
+
---
+++ Sl1pg8r/movies.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'head.html';?>
+</head>
+<body>
+	<!-- 10px top/bottom spacing -->
+	<?php include '/space-between.html';?>
+	
+	<!-- header -->
+	<?php include 'movie-header.html';?>
+	
+	<!-- body content -->
+	<?php include 'section2.html';?>
+	
+	<!-- page footer -->
+	<?php include 'footer.html';?>
+
+<script src="assets/js/app.js"></script>
+</body>
+</html>
+
+
